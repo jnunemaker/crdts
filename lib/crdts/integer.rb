da24eca57eebe4061b07ccbf1e2311b7c35937b5
John Nunemaker <nunemaker@gmail.com>
2012-06-08 20:35:45 -0400

    Added integer type

---
+++ lib/crdts/integer.rb
@@ -0,0 +1,45 @@
+module Crdts
+  class Integer
+    attr_reader :value
+
+    def initialize(value=0)
+      @value = value
+    end
+
+    def increment(increment_by=1)
+      @value += increment_by
+    end
+
+    def decrement(decrement_by=1)
+      @value -= decrement_by
+    end
+
+    def +(other)
+      assert_valid_type(other)
+      duplicate = clone
+      duplicate.increment(other.value)
+      duplicate
+    end
+
+    def -(other)
+      assert_valid_type(other)
+      duplicate = clone
+      duplicate.decrement(other.value)
+      duplicate
+    end
+
+    def eql?(other)
+      self.class.eql?(other.class) && value == other.value
+    end
+
+    def ==(other)
+      eql?(other)
+    end
+
+    private
+
+    def assert_valid_type(other)
+      raise ArgumentError, "other must be a #{self.class.name}" unless other.is_a?(Crdts::Integer)
+    end
+  end
+end
---
+++ spec/crdts/integer_spec.rb
@@ -0,0 +1,135 @@
+require 'crdts/integer'
+
+describe Crdts::Integer do
+  describe "#initialize" do
+    it "defaults value to 0" do
+      integer = Crdts::Integer.new
+      integer.value.should be(0)
+    end
+
+    it "can initialize with different value" do
+      integer = Crdts::Integer.new(5)
+      integer.value.should be(5)
+    end
+  end
+
+  describe "#increment" do
+    context "with value" do
+      it "increments by value" do
+        integer = Crdts::Integer.new(1)
+        integer.increment(3)
+        integer.value.should be(4)
+      end
+    end
+
+    context "without value" do
+      it "increments by 1" do
+        integer = Crdts::Integer.new(1)
+        integer.increment
+        integer.value.should be(2)
+      end
+    end
+  end
+
+  describe "#decrement" do
+    context "with value" do
+      it "decrements by value" do
+        integer = Crdts::Integer.new(5)
+        integer.decrement(3)
+        integer.value.should be(2)
+      end
+    end
+
+    context "without value" do
+      it "decrements by 1" do
+        integer = Crdts::Integer.new(5)
+        integer.decrement
+        integer.value.should be(4)
+      end
+    end
+  end
+
+  describe "#clone" do
+    before do
+      @integer = Crdts::Integer.new(5)
+      @clone = @integer.clone
+    end
+
+    it "returns new instance" do
+      @clone.should_not equal(@integer)
+    end
+
+    it "sets value to the same" do
+      @clone.value.should be(5)
+    end
+  end
+
+  describe "#+" do
+    before do
+      @integer = Crdts::Integer.new(5)
+      @result = @integer + Crdts::Integer.new(2)
+    end
+
+    it "returns new instance" do
+      @result.should be_instance_of(Crdts::Integer)
+      @result.should_not equal(@integer)
+    end
+
+    it "sets value to added values" do
+      @result.value.should be(7)
+    end
+
+    context "with type that is not Crdts::Integer" do
+      it "raises argument error" do
+        expect { @integer + Object.new }.to raise_error(ArgumentError)
+      end
+    end
+  end
+
+  describe "#-" do
+    before do
+      @integer = Crdts::Integer.new(5)
+      @result = @integer - Crdts::Integer.new(2)
+    end
+
+    it "returns new instance" do
+      @result.should be_instance_of(Crdts::Integer)
+      @result.should_not equal(@integer)
+    end
+
+    it "sets value to subtracted values" do
+      @result.value.should be(3)
+    end
+
+    context "with type that is not Crdts::Integer" do
+      it "raises argument error" do
+        expect { @integer - Object.new }.to raise_error(ArgumentError)
+      end
+    end
+  end
+
+  describe "eql?" do
+    it "returns true for same type and value" do
+      integer = Crdts::Integer.new(5)
+      integer.eql?(Crdts::Integer.new(5)).should be_true
+    end
+
+    it "returns false for same type, but different value" do
+      integer = Crdts::Integer.new(5)
+      integer.eql?(Crdts::Integer.new(3)).should be_false
+    end
+
+    it "returns false for different type" do
+      integer = Crdts::Integer.new(5)
+      integer.eql?(Object.new).should be_false
+    end
+  end
+
+  describe "#==" do
+    it "uses eql?" do
+      integer = Crdts::Integer.new(5)
+      integer.should_receive(:eql?)
+      integer == Crdts::Integer.new(5)
+    end
+  end
+end
