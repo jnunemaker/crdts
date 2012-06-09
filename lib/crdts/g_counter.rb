affb4669941a415625df0a8775883850ebe68071
John Nunemaker <nunemaker@gmail.com>
2012-06-08 22:59:20 -0400

    Tomdoc g counter.
    I hate documentation inline with code, but I'm going to give it a try.
    If I continue to hate it after a bit I'll just ax it all.

--- lib/crdts/g_counter.rb
+++ lib/crdts/g_counter.rb
@@ -2,15 +2,43 @@ require 'crdts/integer'
 require 'crdts/replicated_integer_collection'
 
 module Crdts
+
+  # Public: A G-Counter is a grow-only counter (inspired by vector clocks)
+  # in which only increment and merge are possible. Incrementing the counter
+  # adds to the count for the replica provided.
+  #
+  # Examples
+  #
+  #   replica = Crdts::Replica.new('client-1')
+  #   replicated_integer = Crdts::ReplicatedInteger.new(replica)
+  #   collection = Crdts::ReplicatedIntegerCollection.new([replicated_integer])
+  #
+  #   counter = Crdts::GCounter.new(collection)
+  #   counter.increment(replica)
+  #   counter.value # => 1
+  #   counter.increment(replica, 3)
+  #   counter.value # 4
+  #
   class GCounter
+
+    # Public: initialize a new g counter
+    #
+    # collection - A ReplicatedIntegerCollection to operate on
     def initialize(collection = nil)
       @collection = collection || default_collection
     end
 
+    # Public: The sum of the replicated integers
+    #
+    # Returns the integer value of the counter
     def value
       @collection.sum
     end
 
+    # Public: Increment the value for a given replica
+    #
+    # replica - The Replica to increment
+    # increment_by - Integer amount to increment by (default: 1)
     def increment(replica, increment_by=1)
       replicated_integer = @collection.get(replica)
       replicated_integer.increment(increment_by)
